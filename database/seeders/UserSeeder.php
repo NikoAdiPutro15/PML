09d3e55d50719414aabc200d2e7de8a92e011bfb
NikoAdiPutro15 <nikocetot@gmail.com>
2023-11-10 15:12:55 +0700

    meet 4

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,20 +5,34 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
-
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
-        User::factory(5)->create();
+        User::factory(20)->create();
         User::create([
-            'name'=>'Niko Putro',
-            'email'=>'nikoputro15@gmail.com',
-            'email_verified_at' => now(),
-            'password' => ('123456'),
+            'name' => 'Niko',
+            'email' => 'niko@gmail.com',
+            'email_verified_at'  => now(),
+            'role' => 'admin',
+            'phone' => '6285640899224',
+            'bio' => 'flutter dev',
+            'password' => Hash::make('123456'),
         ]);
 
-        
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'email_verified_at'  => now(),
+            'role' => 'superadmin',
+            'phone' => '6285640899225',
+            'bio' => 'laravel dev',
+            'password' => Hash::make('123456'),
+        ]);
     }
 }
