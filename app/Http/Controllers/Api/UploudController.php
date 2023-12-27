3d379fa5bb7c0d4fcbe3a5a3c567de64d632442a
NikoAdiPutro15 <nikocetot@gmail.com>
2023-12-27 17:10:44 +0700

    update midtrans

---
+++ app/Http/Controllers/Api/UploudController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UploadController extends Controller
+{
+    public function uploadImage(Request $request)
+    {
+        if ($request->has('image')) {
+            $image = $request->image;
+            $nameFile = time() . '.' . $image->getClientOriginalExtension();
+            $path = public_path('upload/images');
+            $image->move($path, $nameFile);
+
+            return response()->json([
+                'image_path' => '/upload/images/' . $nameFile,
+                'base_url' => url('/'),
+            ]);
+        }
+    }
+
+}
