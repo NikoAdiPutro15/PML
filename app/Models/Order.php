6da2730ced1ad8a50056510956f999116675fda4
NikoAdiPutro15 <nikocetot@gmail.com>
2024-01-03 15:48:36 +0700

    update midtrans

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id', 'seller_id', 'number', 'total_price',
+        'payment_status', 'snap_url', 'delivery_address',
+        'shipping_cost', 'courier_name',
+    ];
 }
